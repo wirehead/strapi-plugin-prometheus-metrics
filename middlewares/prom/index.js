63411b8e521dad7c8b66a553ac6bc27c36d1886a
Wirehead <wh-github@wirewd.com>
2020-03-14 19:41:53 -0700

    Adding config.

--- middlewares/prom/index.js
+++ middlewares/prom/index.js
@@ -3,9 +3,30 @@
 const apiMetrics = require('prometheus-api-metrics').koaMiddleware;
 
 module.exports = strapi => {
+
   return {
     initialize: function(cb) {
-      strapi.app.use(apiMetrics());
+      const promSettings = strapi.config.middleware.settings.prom;
+      const metricsPath = promSettings.metricsPath;
+      const defaultMetricsInterval = promSettings.defaultMetricsInterval;
+      const durationBuckets = promSettings.durationBuckets;
+      const requestSizeBuckets = promSettings.requestSizeBuckets;
+      const responseSizeBuckets = promSettings.responseSizeBuckets;
+      const useUniqueHistogramName = promSettings.useUniqueHistogramName;
+      const metricsPrefix = promSettings.metricsPrefix;
+      const excludeRoutes = promSettings.excludeRoutes;
+      const includeQueryParams = promSettings.includeQueryParams;
+
+      strapi.app.use(apiMetrics({
+        metricsPath,
+        defaultMetricsInterval,
+        durationBuckets,
+        requestSizeBuckets,
+        responseSizeBuckets,
+        useUniqueHistogramName,
+        excludeRoutes,
+        includeQueryParams
+      }));
     },
   };
 };
